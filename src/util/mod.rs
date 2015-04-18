d2bf77beaba7bf06560ef8186ec767a5f0d964f4
Nemikolh <joan.varvenne@gmail.com>
2015-04-18 23:14:00 +0100

    Added temporary wrapper to make f32 verify Ord.
    Once max_by will be updated, this wrapper will be removed.
    See rust-lang/rust#15311

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,2 +1,21 @@
+use std::cmp::Ord;
+use std::cmp::Ordering;
 
 pub mod flat_tree;
+
+#[derive(PartialOrd, PartialEq)]
+pub struct F32Ord(pub f32);
+
+impl Ord for F32Ord {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.0 < other.0 {
+            Ordering::Less
+        } else if self.0 == other.0 {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
+    }
+}
+
+impl Eq for F32Ord {}
