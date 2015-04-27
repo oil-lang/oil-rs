32726ccad798283baa78420789fa536b69780a38
Vaelden <maugan.villatel@gmail.com>
2015-04-27 10:19:55 +0100

    Simple data bindings should now work
    Single values and objects are supported
    Single values are masked by objects
    Global objects and values are masked by view-specific object and values
    The <repeat> tag is still not supported

--- src/data_bindings.rs
+++ src/data_bindings.rs
@@ -1,16 +1,59 @@
 use std::rc::{Rc,Weak};
-use std::collections::hash_map::{HashMap,Entry};
+use std::collections::HashMap;
 use std::collections::hash_state::HashState;
 use std::cell::RefCell;
 
 use router::Router;
 
-#[derive(Clone,Debug)]
-pub struct StoreValue {
-    last_changed: usize,
+macro_rules! declare_data_binding {
+    ($name:ident {
+        $($field:ident:$type_field:ty),*
+    }) => (
+        declare_data_binding! {
+            $name {
+                $($field -> $field: $type_field),*
+            }
+        }
+        );
+    ($name:ident {
+        $($key:ident -> $field:ident : $type_field:ty),*
+    }) => (
+        impl DBStore for $name {
+            fn get_value(&self, k: &str) -> Option<StoreValue> {
+                match k {
+                    $(stringify!($key) => Some(StoreValue{data: self.$field.to_string()}),)*
+                    _ => None,
+                }
+            }
+            fn set_value(&mut self, k: &str, value: StoreValue) -> Option<StoreValue> {
+                use std::str::FromStr;
+                match k {
+                    $(stringify!($key) => {
+                        match <$type_field as FromStr>::from_str(&value.data) {
+                            Ok(value) => {
+                                self.$field = value;
+                                None
+                            }
+                            Err(..) => {
+                                println!("WARNING: Failed to parse data binding {} as {}", &value.data, stringify!($type_field));
+                                Some(value)
+                            }
+                        }
+                    })*
+                    _ => Some(value),
+                }
+            }
+        }
+
+        )
 }
 
-pub trait DBStore {
+#[derive(Clone,Debug)]
+pub struct StoreValue {
+    data: String,
+}
+
+pub trait DBStore: ::std::fmt::Debug {
     fn get_value(&self, k: &str) -> Option<StoreValue>;
     fn set_value(&mut self, k: &str, value: StoreValue) -> Option<StoreValue>;
 }
@@ -65,11 +108,61 @@ impl <'a> DBStore for Box<DBStore + 'a> {
     }
 }
 
+#[derive(Default,Debug)]
+struct DataBinderScope {
+    values: HashMap<String,StoreValue>,
+    stores: HashMap<String,Box<DBStore>>,
+}
+
+impl DBStore for DataBinderScope {
+    fn get_value(&self, k: &str) -> Option<StoreValue> {
+        for (prefix, key) in PrefixKeyIterator::new(k) {
+            if let Some(store) = self.stores.get(prefix) {
+                // If we have a store registered, we look here first
+                let result = store.get_value(key);
+                if result.is_some() {
+                    return result;
+                }
+            }
+        }
+        self.values.get_value(k)
+    }
+
+    fn set_value(&mut self, k: &str, mut value: StoreValue) -> Option<StoreValue> {
+        for (prefix, key) in PrefixKeyIterator::new(k) {
+            if let Some(store) = self.stores.get_mut(prefix) {
+                // If we have a store registered, we look here first
+                match store.set_value(key, value) {
+                    None => return None,
+                    Some(ret) => value = ret,
+                }
+            }
+        }
+        self.values.set_value(k, value)
+    }
+}
+
+impl DataBinderScope {
+    fn register_value(&mut self, key: String, value: StoreValue) -> Result<(),StoreValue> {
+        match self.values.insert(key, value) {
+            Some(old) => Err(old),
+            None => Ok(()),
+        }
+    }
+
+    fn register_store(&mut self, prefix: String, store: Box<DBStore + 'static>)
+        -> Result<(),Box<DBStore + 'static>> {
+        match self.stores.insert(prefix, store) {
+            Some(old) => Err(old),
+            None => Ok(()),
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct DataBinderContext {
-    global_values: HashMap<String,StoreValue>,
-    global_context: Vec<Box<DBStore>>,
-    views_context: HashMap<String,Vec<Box<DBStore>>>,
+    global: DataBinderScope,
+    views: HashMap<String,DataBinderScope>,
     current_view: String,
 }
 
@@ -81,44 +174,59 @@ impl DataBinderContext {
     }
 
     pub fn register_views(&mut self, router: &Router) {
-        // XXX: Could have better implementation: use Entry
         for name in router.iter_name_views() {
-            self.views_context.entry(name.clone()).or_insert(Vec::new());
+            self.register_view(name.clone());
         }
     }
 
-    pub fn register_global_single_value(&mut self, key: String, value: StoreValue) {
-        // XXX: Any better way to handle the log?
-        match self.global_values.entry(key.clone()) {
-            Entry::Occupied(mut occupied) => {
-                // TODO: Use log
-                println!("WARNING: registering an already existing value for key {} : {:?}", key, occupied.get());
-                occupied.insert(value);
-            }
-            Entry::Vacant(v) => {
-                v.insert(value);
-            }
+    pub fn switch_to_view(&mut self, view: String) {
+        self.current_view = view;
+    }
+
+    pub fn register_global_value(&mut self, key: String, value: StoreValue) {
+        if let Err(old) = self.global.register_value(key.clone(), value) {
+            println!("WARNING: re-registering global value {} (old value {:?})", key, old);
         }
     }
 
-    pub fn register_global<T>(&mut self, scope: usize, value: Rc<RefCell<T>>)
+    pub fn register_global_store<T>(&mut self, prefix: String, value: Rc<RefCell<T>>)
     where T: DBStore + 'static {
         let v = Box::new(Proxy::new(value));
-        let len = self.global_context.len();
-        self.global_context.insert(::std::cmp::max(scope, len), v);
+        if let Err(old) = self.global.register_store(prefix.clone(), v) {
+            println!("WARNING: overriding global object {} (old value {:?})", prefix, old);
+        }
     }
 
-    pub fn register_for_view<T>(&mut self, view: &str, scope: usize, value: Rc<RefCell<T>>) -> Result<(),String>
-    where T: DBStore + 'static {
-        let v = Box::new(Proxy::new(value));
-        let len = self.global_context.len();
-        match self.views_context.get_mut(view) {
+    pub fn register_value(&mut self, view: &str, key: String, value: StoreValue) -> Result<(),String> {
+        match self.views.get_mut(view) {
             None => Err(format!("Could not find view {}", view)),
-            Some(vec) => {
-                vec.insert(::std::cmp::max(scope, len), v);
+            Some(view_scope) => {
+                if let Err(old) = view_scope.register_value(key.clone(), value) {
+                    // Don't throw an error, just print a warning
+                    println!("WARNING: View {}: re-registering value {} (old value {:?})", view, key, old);
+                }
                 Ok(())
             }
         }
+    }
+
+    pub fn register_store<T>(&mut self, view: &str, prefix: String, value: Rc<RefCell<T>>) -> Result<(),String>
+    where T: DBStore + 'static {
+        match self.views.get_mut(view) {
+            None => Err(format!("Could not find view {}", view)),
+            Some(view_scope) => {
+                let v = Box::new(Proxy::new(value));
+                if let Err(old) = view_scope.register_store(prefix.clone(), v) {
+                    // Don't throw an error, just print a warning
+                    println!("WARNING: View {}: overriding object {} (old value {:?})", view, prefix, old);
+                }
+                Ok(())
+            }
+        }
+    }
+
+    fn register_view(&mut self, view: String) {
+        self.views.entry(view).or_insert_with(|| DataBinderScope::default());
     }
 }
 
@@ -130,7 +238,7 @@ pub trait DBCLookup {
 
 impl DBCLookup for DataBinderContext {
     fn get_value(&self, k: &str) -> Option<StoreValue> {
-        match self.views_context.get(&self.current_view) {
+        match self.views.get(&self.current_view) {
             None => {
                 println!("WARNING: Did not find view {}", &self.current_view);
             }
@@ -142,15 +250,11 @@ impl DBCLookup for DataBinderContext {
             }
         }
         // Did not find view, or view did not have the corresponding value
-        let result = self.global_context.get_value(k);
-        if result.is_some() {
-            return result;
-        }
-        self.global_values.get_value(k)
+        self.global.get_value(k)
     }
 
     fn set_value(&mut self, k: &str, value: StoreValue) {
-        match self.views_context.get_mut(&self.current_view) {
+        match self.views.get_mut(&self.current_view) {
             None => {
                 println!("WARNING: Did not find view {}", &self.current_view);
             }
@@ -158,11 +262,8 @@ impl DBCLookup for DataBinderContext {
                 let result = view_store.set_value(k, value);
                 match result {
                     None => {},
-                    Some(a) => {
-                        match self.global_context.set_value(k, a) {
-                            None => {},
-                            Some(a) => {self.global_values.set_value(k, a);}
-                        }
+                    Some(value) => {
+                        self.global.set_value(k, value);
                     }
                 }
             }
@@ -170,6 +271,7 @@ impl DBCLookup for DataBinderContext {
     }
 }
 
+#[derive(Debug)]
 struct Proxy<T: DBStore> {
     data: Weak<RefCell<T>>,
 }
@@ -196,5 +298,152 @@ impl <T: DBStore> Proxy<T> {
         Proxy {
             data: value.downgrade(),
         }
+    }
+}
+
+// An iterator over the different combinations of prefix-key
+struct PrefixKeyIterator<'a> {
+    data: &'a str,
+    position: i8,
+}
+
+impl <'a> PrefixKeyIterator<'a> {
+    fn new(data: &'a str) -> PrefixKeyIterator<'a> {
+        PrefixKeyIterator {
+            data: data,
+            position: 0,
+        }
+    }
+}
+
+impl <'a> Iterator for PrefixKeyIterator<'a> {
+    type Item = (&'a str, &'a str);
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        if self.position == 0 {
+            self.position += 1;
+            return Some(("", self.data));
+        }
+        let mut position = self.position;
+        self.position += 1;
+        let mut iterator = self.data.split(|c| {
+            if c == '.' {
+                position -= 1;
+                if position == 0 {
+                    return true;
+                }
+            }
+            false
+        });
+        let prefix = match iterator.next() {
+            None => return None,
+            Some(a) => a,
+        };
+        let key = match iterator.next() {
+            None => return None,
+            Some(a) => a,
+        };
+        Some((prefix,key))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+    use super::*;
+#[derive(Debug)]
+    struct Player {
+        pv: usize,
+        xp: usize,
+    }
+
+    declare_data_binding! {
+        Player {
+            pv: usize,
+            xp: usize
+        }
+    }
+
+    #[test]
+    fn register_global_player() {
+        let mut context = DataBinderContext::default();
+        let player = Rc::new(RefCell::new(Player{pv: 42, xp: 100}));
+        // Clone the Rc, as it will get downgraded to Weak when registered
+        context.register_global_store("player".to_string(), player.clone());
+        assert_eq!(context.get_value("player.pv").unwrap().data, "42");
+        assert_eq!(context.get_value("player.xp").unwrap().data, "100");
+    }
+
+    #[test]
+    fn register_global_value() {
+        let mut context = DataBinderContext::default();
+        context.register_global_value("option.width".to_string(),
+            StoreValue{data: "42".to_string()});
+        assert_eq!(context.get_value("option.width").unwrap().data, "42");
+    }
+
+    #[test]
+    fn register_view_player() {
+        let mut context = DataBinderContext::default();
+        context.register_view("foo".to_string());
+        let player = Rc::new(RefCell::new(Player{pv: 42, xp: 100}));
+        // Clone the Rc, as it will get downgraded to Weak when registered
+        context.register_store("foo", "player".to_string(), player.clone()).unwrap();
+
+        // Not in the correct view
+        assert!(context.get_value("player.pv").is_none());
+        context.switch_to_view("foo".to_string());
+        assert_eq!(context.get_value("player.pv").unwrap().data, "42");
+        assert_eq!(context.get_value("player.xp").unwrap().data, "100");
+    }
+
+    #[test]
+    fn register_view_value() {
+        let mut context = DataBinderContext::default();
+        context.register_view("foo".to_string());
+        context.register_value("foo", "option.width".to_string(),
+            StoreValue{data: "42".to_string()}).unwrap();
+        assert!(context.get_value("option.width").is_none());
+        context.switch_to_view("foo".to_string());
+        assert_eq!(context.get_value("option.width").unwrap().data, "42");
+    }
+
+    #[test]
+    fn masking_value_by_object() {
+        let mut context = DataBinderContext::default();
+        context.register_global_value("player.pv".to_string(),
+            StoreValue{data: "12".to_string()});
+        assert_eq!(context.get_value("player.pv").unwrap().data, "12");
+        let player = Rc::new(RefCell::new(Player{pv: 42, xp: 100}));
+        // Clone the Rc, as it will get downgraded to Weak when registered
+        context.register_global_store("player".to_string(), player.clone());
+        assert_eq!(context.get_value("player.pv").unwrap().data, "42");
+    }
+
+    #[test]
+    fn masking_global_value_by_view() {
+        let foo = "foo".to_string();
+        let bar = "bar".to_string();
+        let mut context = DataBinderContext::default();
+        context.register_view(foo.clone());
+        context.register_view(bar.clone());
+        context.register_view("foobar".to_string());
+        context.register_value("foo", "option.width".to_string(),
+            StoreValue{data: "foo_value".to_string()}).unwrap();
+        context.register_value("bar", "option.width".to_string(),
+            StoreValue{data: "bar_value".to_string()}).unwrap();
+        context.register_global_value("option.width".to_string(),
+            StoreValue{data: "global_value".to_string()});
+
+        // In view "foobar" -> get global value
+        context.switch_to_view("foobar".to_string());
+        assert_eq!(context.get_value("option.width").unwrap().data, "global_value");
+        // In view "foo" -> get foo specific value
+        context.switch_to_view("foo".to_string());
+        assert_eq!(context.get_value("option.width").unwrap().data, "foo_value");
+        // In view "bar" -> get bar specific value
+        context.switch_to_view("bar".to_string());
+        assert_eq!(context.get_value("option.width").unwrap().data, "bar_value");
     }
 }
