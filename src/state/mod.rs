3e59b0a295d1ef7ba032f7428a3086f77e9611b6
Nemikolh <joan.varvenne@gmail.com>
2015-05-02 11:47:27 +0100

    Added set_current_state to StateData.

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -48,6 +48,7 @@ pub struct StateData {
     focus_properties: HashMap<PropertyName, Value>,
     hover_properties: HashMap<PropertyName, Value>,
     creation_properties: HashMap<PropertyName, Value>,
+    current_state: SelectorState,
 }
 
 // ======================================== //
@@ -59,16 +60,35 @@ pub struct StateData {
 macro_rules! return_length_or_zero {
 
     ($this:ident try $prop_name:ident) => {
-        return_length_or_zero!(rec $this try $prop_name else { 0f32 });
+        return_length_or_zero!(rec $this try $prop_name else {
+            return_length_or_zero!(rec_default $this try $prop_name else { 0f32 })
+        });
     };
 
     ($this:ident try $prop_name:ident else $other:ident) => {
         return_length_or_zero!(rec $this try $prop_name else {
-            return_length_or_zero!(rec $this try $other else { 0f32 })
+            return_length_or_zero!(rec $this try $other else {
+                return_length_or_zero!(rec_default $this try $prop_name else {
+                    return_length_or_zero!(rec_default $this try $other else { 0f32 })
+                })
+            })
         });
     };
 
     (rec $this:ident try $prop_name:ident else $none_case:block) => {
+        match $this.current_properties().get(&$prop_name) {
+            Some(v) => {
+                if let Value::Length(val, Unit::Px) = *v {
+                    val
+                } else {
+                    0f32
+                }
+            }
+            None => $none_case
+        }
+    };
+
+    (rec_default $this:ident try $prop_name:ident else $none_case:block) => {
         match $this.default_properties.get(&$prop_name) {
             Some(v) => {
                 if let Value::Length(val, Unit::Px) = *v {
@@ -91,11 +111,16 @@ impl StateData {
             focus_properties: HashMap::new(),
             hover_properties: HashMap::new(),
             creation_properties: HashMap::new(),
+            current_state: SelectorState::Default
         };
 
         state.set_properties(node, style);
 
         state
+    }
+
+    pub fn set_current_state(&mut self, new_state: SelectorState) {
+        self.current_state = new_state;
     }
 
     pub fn has_property_expand(&self, prop_name: PropertyName) -> bool {
@@ -107,34 +132,49 @@ impl StateData {
     }
 
     pub fn has_property_eq_kw(&self, prop_name: PropertyName, kw: KwValue) -> bool {
-        match self.default_properties.get(&prop_name) {
+        match self.current_properties().get(&prop_name) {
             Some(&Value::Keyword(v)) if v == kw => {
                 true
             },
-            _ => false
+            _ => match self.default_properties.get(&prop_name) {
+                Some(&Value::Keyword(v)) if v == kw => {
+                    true
+                },
+                _ => false
+            }
         }
     }
 
     pub fn get_background_rule(&self) -> Option<TextureRule> {
-        match self.default_properties.get(&PropertyName::BACKGROUND_IMAGE_RULE) {
+        match self.current_properties().get(&PropertyName::BACKGROUND_IMAGE_RULE) {
             Some(&Value::Keyword(v)) => match v {
                 KwValue::Fit => Some(TextureRule::Fit),
                 KwValue::Repeat => Some(TextureRule::Repeat),
                 _ => None
             },
-            _ => None
+            _ => match self.default_properties.get(&PropertyName::BACKGROUND_IMAGE_RULE) {
+                Some(&Value::Keyword(v)) => match v {
+                    KwValue::Fit => Some(TextureRule::Fit),
+                    KwValue::Repeat => Some(TextureRule::Repeat),
+                    _ => None
+                },
+                _ => None
+            }
         }
     }
 
     pub fn get_background_image(&self) -> Option<ImageData> {
-        match self.default_properties.get(&PropertyName::BACKGROUND_IMAGE) {
+        match self.current_properties().get(&PropertyName::BACKGROUND_IMAGE) {
             Some(&Value::Image(ref id)) => Some(id.clone()),
-            _ => None
+            _ => match self.default_properties.get(&PropertyName::BACKGROUND_IMAGE) {
+                Some(&Value::Image(ref id)) => Some(id.clone()),
+                _ => None
+            }
         }
     }
 
     pub fn size_prop_as_opt(&self, prop_name: PropertyName) -> Option<f32> {
-        match self.default_properties.get(&prop_name) {
+        match self.current_properties().get(&prop_name) {
             Some(v) => {
                 if let Value::Length(val, Unit::Px) = *v {
                     Some(val)
@@ -142,7 +182,16 @@ impl StateData {
                     None
                 }
             }
-            None => None
+            None => match self.default_properties.get(&prop_name) {
+                Some(v) => {
+                    if let Value::Length(val, Unit::Px) = *v {
+                        Some(val)
+                    } else {
+                        None
+                    }
+                }
+                None => None
+            }
         }
     }
 
@@ -179,6 +228,15 @@ impl StateData {
                 return_length_or_zero!(self try prop_name else BORDER)
             }
             _ => panic!()
+        }
+    }
+
+    fn current_properties<'a>(&'a self) -> &HashMap<PropertyName, Value> {
+        match self.current_state {
+            SelectorState::Focus => &self.focus_properties,
+            SelectorState::Hover => &self.hover_properties,
+            SelectorState::Creation => &self.creation_properties,
+            SelectorState::Default => &self.default_properties,
         }
     }
 
